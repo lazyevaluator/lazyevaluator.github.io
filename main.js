19faca3cda82765e04c87efde37dc13d5dabdecd
lazyevaluator <58919169+lazyevaluator@users.noreply.github.com>
2024-09-05 16:19:58 +0200

    add draw feature + change name

--- main.js
+++ main.js
@@ -4,5 +4,36 @@ canvas.width = window.innerWidth
 
 const context = canvas.getContext("2d")
 
-context.fillStyle = "red"
-context.fillRect(100, 100, 100, 100)
+let prevX = null
+let prevY = null
+
+
+context.lineWidth = 5
+
+let draw = false
+
+window.addEventListener("mousedown", (e) => draw = true)
+
+window.addEventListener("mouseup", (e) => draw = false)
+
+window.addEventListener("mousemove", (e) => {
+
+    if (prevX == null || prevY == null || !draw){
+        prevX = e.clientX
+        prevY = e.clientY
+        return
+    }
+
+    let currentX = e.clientX
+    let currentY = e.clientY
+
+    context.beginPath()
+    context.moveTo(prevX, prevY)
+    context.lineTo(currentX, currentY)
+    context.stroke()
+
+    prevX = currentX
+    prevY = currentY
+    console.log("Mouse X: " + e.clientX)
+    console.log("MouseY: " + e.clientY)
+})
